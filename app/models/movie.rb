9b284f0dab0e7d340bdd438f375c8dd6220f12d9
Ian Whitney <iwhitney@ssa-i.org>
2013-01-05 18:46:44 -0600

    consolidating the use of imdb uri

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -5,7 +5,10 @@ class Movie < RedisBase
     $redis.get(self.id).to_i
   end
 
-  def poster
-    @poster_uri if @poster_uri.match(/http/)
+  def poster_uri
+    AWS::S3::Base.establish_connection!(:access_key_id => ENV["S3_ACCESS_KEY_ID"],:secret_access_key => ENV["S3_SECRET_ACCESS_KEY"])
+    if S3Object.exists?("#{self.id}.jpg", ENV['S3_BUCKET'])
+      S3Object.url_for("#{self.id}.jpg", ENV['S3_BUCKET'])
+    end
   end
 end
--- app/models/recommendation_display.rb
+++ app/models/recommendation_display.rb
@@ -11,9 +11,8 @@ class RecommendationDisplay
   end
 
   def image
-    AWS::S3::Base.establish_connection!(:access_key_id => ENV["S3_ACCESS_KEY_ID"],:secret_access_key => ENV["S3_SECRET_ACCESS_KEY"])
-    if S3Object.exists?("#{@movie.id}.jpg", ENV['S3_BUCKET'])
-      S3Object.url_for("#{@movie.id}.jpg", ENV['S3_BUCKET'])
+    if @movie.poster_uri
+      @movie.poster_uri
     else
       "/images/nopicture.gif"
     end
