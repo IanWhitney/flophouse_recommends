37b163250684786b9bab9bbcfb403d1ba94fc9b7
Ian Whitney <iwhitney@ssa-i.org>
2013-01-07 09:45:43 -0600

    removing hardcoded bucket name

--- app/models/remote_file.rb
+++ app/models/remote_file.rb
@@ -7,7 +7,7 @@ class RemoteFile
     #AWS::S3::Base.establish_connection!(:access_key_id => ENV["S3_ACCESS_KEY_ID"],:secret_access_key => ENV["S3_SECRET_ACCESS_KEY"])
 
     #if S3Object.exists?(file_name, ENV['S3_BUCKET'])
-      @url = "http://s3.amazonaws.com/the.flophouse.recommends/#{file_name}"
+      @url = "http://s3.amazonaws.com/#{ENV['S3_BUCKET']}/#{file_name}"
     #end
   end
 end
