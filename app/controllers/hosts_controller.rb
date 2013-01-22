9f2329ab026c01dc1eca1c54a16d582e71f3856e
Ian Whitney <iwhitney@ssa-i.org>
2013-01-22 09:28:55 -0600

    removing unused controller

--- app/controllers/hosts_controller.rb
+++ app/controllers/hosts_controller.rb
@@ -1,6 +1,5 @@
 class HostsController < ApplicationController
   def show
     @host = Host.find(params[:id])
-    @episodes = Episodes.by_host(@host).all
   end
 end
--- app/controllers/main_controller.rb
+++
@@ -1,9 +0,0 @@
-class MainController < ApplicationController
-  def index
-    @episode = Episode.all.reverse[0]
-  end
-
-  def show
-    @episode = Episode.find(params[:id])
-  end
-end
