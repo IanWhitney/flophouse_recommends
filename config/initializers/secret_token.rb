efb67807b082e65526cf3240c3fff68a66f8bb06
Ian Whitney <iwhitney@ssa-i.org>
2013-02-03 12:13:31 -0600

    Moving newly-generated secret token to a environment variable

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-FlophouseRecommends::Application.config.secret_token = 'e961590a07711f4c7eb347ad53d0cae5fa230eb63d2a03e9b9a779f12cc39ae684771871a08bb3b028fe9deaa02f9de98d9688be86082643e65ac60ef9650499'
+FlophouseRecommends::Application.config.secret_token = ENV['SECRET_TOKEN']
