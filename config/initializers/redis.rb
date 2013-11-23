de25cbc945a3947418a9bd6a448875ef521832f5
Ian Whitney <whit0694@umn.edu>
2013-11-23 09:34:30 -0600

    redis debugging

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,12 +1,6 @@
 # uri = URI.parse(ENV["REDISTOGO_URL"])
 # $redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
-if Rails.env.production?
-  puts "Hey, the variable is #{ENV['REDISTOGO_URL']}"
+if ENV["REDISTOGO_URL"]
   uri = URI.parse(ENV["REDISTOGO_URL"])
   $redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
-else
-  uri = URI.parse(ENV["REDISTOGO_URL"])
-  $redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
-   #$redis = Redis.new(:host => 'localhost', :port => 6379)
-   #$redis.select(1)
 end
