266ee6df05c7dde0632ef1698c0d0af9abc151d5
Ian Whitney <whit0694@umn.edu>
2013-11-23 09:37:50 -0600

    the redis initializer will fail during a heroku deploy because it's called before environment variables are set. fixed.

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,5 +1,3 @@
-# uri = URI.parse(ENV["REDISTOGO_URL"])
-# $redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
 if ENV["REDISTOGO_URL"]
   uri = URI.parse(ENV["REDISTOGO_URL"])
   $redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
