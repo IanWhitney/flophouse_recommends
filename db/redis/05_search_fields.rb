a653033f333b39704d1f6b8ba51e1798d57dca55
Ian Whitney <whit0694@umn.edu>
2015-03-15 19:22:55 -0500

    search updating now episode specific

--- db/redis/05_search_fields.rb
+++ db/redis/05_search_fields.rb
@@ -1,3 +1,8 @@
-Movie.all.each do |m|
+require 'csv'
+
+@recommendations = @episode ? Episode.find(@episode).recommendations.to_a : Recommendation.all
+
+@recommendations.each do |recommendation|
+  m = Movie.find(recommendation.imdb_id)
   $redis.set "title_search:#{m.title.downcase}", m.id
 end
