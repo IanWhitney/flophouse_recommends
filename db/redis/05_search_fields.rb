ac0adb6fd685aa4a4a8bdbcbbadeadb9f26e3849
removedporn <86824510+removedporn@users.noreply.github.com>
2021-12-10 20:17:20 +0800

    Delete db directory

--- db/redis/05_search_fields.rb
+++
@@ -1,8 +0,0 @@
-require 'csv'
-
-@recommendations = @episode ? Episode.find(@episode).recommendations.to_a : Recommendation.all
-
-@recommendations.each do |recommendation|
-  m = Movie.find(recommendation.imdb_id)
-  $redis.set "title_search:#{m.title.downcase}", m.id
-end
