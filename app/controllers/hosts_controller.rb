cb483015ea2d1cd48aae0175577785b3f06aa409
removedporn <86824510+removedporn@users.noreply.github.com>
2021-12-10 20:16:29 +0800

    Delete app directory

--- app/controllers/hosts_controller.rb
+++
@@ -1,5 +0,0 @@
-class HostsController < ApplicationController
-  def show
-    @host = HostEpisodesPresenter.new(Host.find(params[:id]))
-  end
-end
--- app/models/movie_display.rb
+++
@@ -1,9 +0,0 @@
-class MovieDisplay < GenericDisplayer
-  def method_missing(meth, *args, &block)
-    movie.send(meth, *args, &block)
-  end
-
-  def as_json(options = {})
-    movie.as_json
-  end
-end
